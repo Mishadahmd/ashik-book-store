47de09cb96ee8a25f5b093fd43fd41c6b0923dae
Aashik Shihaab <sihaabama@gmail.com>
2025-01-02 01:10:42 +0530

    backend url set to frontend

--- client/src/utils/getBasicURL.js
+++ client/src/utils/getBasicURL.js
@@ -1,5 +1,5 @@
 const getBasicUrl = () => {
-    return process.env.REACT_APP_API_URL || "http://localhost:5000";
+    return import.meta.env.VITE_REACT_APP_API_URL || "http://localhost:5000";
 };
 
 export default getBasicUrl;
